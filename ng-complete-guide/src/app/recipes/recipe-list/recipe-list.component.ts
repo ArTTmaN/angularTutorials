c1ab8e765e8a80bbb8ab95123beb378a5df7f7a5
Artjom Mashanin <arttmanz@gmail.com>
2017-08-29 14:12:59 +0300

    Added recipe model and updated several html files

--- ng-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
+++ ng-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'http://maxpixel.freegreatpicture.com/static/photo/1x/Cooking-Delicious-Korean-Food-Recipe-Asia-822090.jpg'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'http://maxpixel.freegreatpicture.com/static/photo/1x/Cooking-Delicious-Korean-Food-Recipe-Asia-822090.jpg')
+  ];
 
   constructor() { }
 
