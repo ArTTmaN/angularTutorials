70bb519ae3367b82e128cef61ef35b3f9c745370
Artjom <armasa@ttu.ee>
2017-09-17 13:57:34 +0300

    finished lecture 126

--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -15,7 +15,7 @@ export class ServerComponent implements OnInit {
               private route: ActivatedRoute) { }
 
   ngOnInit() {
-    // + is to convert to a number
+    //  + is to convert to a number
     const id = +this.route.snapshot.params['id'];
     this.server = this.serversService.getServer(id);
     this.route.params.subscribe(
