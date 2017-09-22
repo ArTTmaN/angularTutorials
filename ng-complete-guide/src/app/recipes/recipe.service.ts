226d10684b5d24b921f6ff52c79a7a07dada8879
Artjom <armasa@ttu.ee>
2017-09-22 23:01:57 +0300

    finished lecture 156

--- ng-complete-guide/src/app/recipes/recipe.service.ts
+++ ng-complete-guide/src/app/recipes/recipe.service.ts
@@ -10,16 +10,16 @@ export class RecipeService {
 
   private recipes: Recipe[] = [
     new Recipe(
-      'A Test Recipe', 
-      'This is simply a test', 
+      'A Test Recipe',
+      'This is simply a test',
       'http://maxpixel.freegreatpicture.com/static/photo/1x/Cooking-Delicious-Korean-Food-Recipe-Asia-822090.jpg',
       [
         new Ingredient('Meat', 1),
         new Ingredient('French Fries', 20)
       ]),
     new Recipe(
-      'Another Test Recipe', 
-      'This is simply a test', 
+      'Another Test Recipe',
+      'This is simply a test',
       'http://maxpixel.freegreatpicture.com/static/photo/1x/Cooking-Delicious-Korean-Food-Recipe-Asia-822090.jpg',
       [
         new Ingredient('Buns', 2),
@@ -30,8 +30,12 @@ export class RecipeService {
   constructor(private slService: ShoppingListService) {}
 
   getRecipes() {
-    // return an exact copy of that array. Not a reference to that array
+    // returns an exact copy of that array. Not a reference to that array
     return this.recipes.slice();
+  }
+
+  getRecipe(index: number) {
+    return this.recipes[index];
   }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
