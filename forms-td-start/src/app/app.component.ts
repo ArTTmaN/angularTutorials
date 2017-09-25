98323711c5b8a8bd5886d1d270bbfcb5ac1e90a1
Artjom <armasa@ttu.ee>
2017-09-25 20:09:56 +0300

    finished lecture 185

---
+++ forms-td-start/src/app/app.component.ts
@@ -0,0 +1,59 @@
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from "@angular/forms";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  @ViewChild('f') signupForm: NgForm;
+  defaultSelect = 'pet';
+  answer = '';
+  genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+    // This will rewrite all the data in the form
+    // And we need to recreate a complete object to pass to this function
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+
+    // This allows to set only needed values and leave others unchanged
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
+  }
+
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+
+  onSubmit() {
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+    this.submitted = true;
+
+    this.signupForm.reset();
+  }
+}
